f8c31f1a62d525214bbc97ff020eb7b06e98c358
Elaanor <lol_34_34@hotmail.com>
2022-03-04 16:07:28 +0300

    nodejs

---
+++ index.js
@@ -0,0 +1,12 @@
+// const fsPromise = require('fs/promises');
+const fsPromise = require('fs').promises;
+
+const fileRead = async () => {
+    try {
+        await fsPromise.writeFile(path.resolve(__dirname,"text1.txt"),'data');
+
+
+    } catch (error) {
+        
+    }
+}
